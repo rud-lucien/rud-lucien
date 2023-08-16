d683b0ca44c03a8cdf238fa53400fcf3aded5721
rud-lucien <30354417+rud-lucien@users.noreply.github.com>
2023-08-16 11:15:01 -0400

    parsing is now working
    made changes to SerialPortReader to get rid of parsing function argument. also I have a version of test_script that is working

--- julia-scripts/microcontorller-script/SerialPortReader.jl
+++ julia-scripts/microcontorller-script/SerialPortReader.jl
@@ -47,33 +47,34 @@ module SerialPortReader
     end
 
     """
-        start_reading(portname::String, baudrate::Int, parse_func=println)
+    start_reading(portname::String, baudrate::Int; print_data_to_console::Bool=false)
 
     Starts reading data from the serial port specified by `portname` at the given `baudrate`. 
-    The data is parsed according to `parse_func`, which should be a function that takes an array of 
-    substrings as input and processes it in some way. If `parse_func` is set to `nothing`, the data 
-    is read but not processed.
+    This function starts two asynchronous tasks: one for reading the data from the serial port 
+    and another for processing the data lines. If `print_data_to_console` is set to true, the 
+    read data is printed to the console.
 
     # Arguments
     - `portname`: Name of the serial port (String)
     - `baudrate`: Baud rate for the serial communication (Int)
-    - `parse_func`: Function to parse the read data. Should take an array of substrings as input 
-      and process it in some way. If set to `nothing`, the data is read but not processed. 
-      (Function, default=println)
+    - `print_data_to_console`: If set to true, the read data is printed to the console. (Bool, default=false)
 
     # Returns
-    - `nothing`
+    - A Channel that the read data is pushed into
     """
-    function start_reading(portname::String, baudrate::Int, parse_func=println)
+    function start_reading(portname::String, baudrate::Int; print_data_to_console::Bool=false)
         global continue_reading = true
         ch = Channel{Array{SubString{String},1}}(Inf)  # Create a channel to store the data lines
+        data_ch = Channel{Array{SubString{String},1}}(Inf)  # Create a channel to return the data
         @async read_serial_data(ch, portname, baudrate)  # Run read_serial_data in a separate task
         @async while continue_reading  # Process the data lines in another task
             data_line = take!(ch)  # Take a data line from the channel
-            if parse_func !== nothing
-                parse_func(data_line)  # Call the parsing function with the data line
+            if print_data_to_console
+                println(data_line)  # print the data line to the console
             end
+            put!(data_ch, data_line)  # put the data line into the data channel
         end
+        return data_ch  # return the data channel
     end
 
     """
--- julia-scripts/microcontorller-script/test_scripts.jl
+++ julia-scripts/microcontorller-script/test_scripts.jl
@@ -2,77 +2,76 @@
 using GLMakie
 using Observables
 using DataStructures: CircularBuffer
+include("SerialPortReader.jl")
 using .SerialPortReader  # Import your SerialPortReader module
+using Dates
 
-# Create a counter variable
-counter_increment = 0
-counter_reset = 0
+
+# Define a global variable to control the viewing process
+global continue_viewing = true
 
 """
-    parse_increment(line::Array{SubString{String},1})
+    parse_data(data::Array{SubString{String},1})
 
-Parse a line of data values. Only the last four values are parsed. Increments the timestamp counter.
+Parse the data read from the serial port. This function needs to be implemented.
 
 # Arguments
-- `line`: An array of substrings
+- `data`: Data read from the serial port (Array{SubString{String},1})
 
 # Returns
-- An array of parsed float values with timestamp as the first element
+- Parsed data
 """
-function parse_increment(line::Array{SubString{String},1})
-    global counter_increment
-    counter_increment += 1  # Increment the counter
-    parsed_values_increment = parse.(Float64, line[end-3:end])  # Convert to float array
-    result_increment = [counter_increment; parsed_values_increment]  # Create result array with counter as the first element
-    println("Result Increment: ", result_increment)  # Print the result
-    return result_increment
+function parse_data(data::Array{SubString{String},1})
+    # Remove any non-numeric strings
+    numeric_data = filter(x -> isdigit(x[1]) || x[1] == '.', data)
+
+    # Convert each string in the array to a float
+    float_data = map(x -> parse(Float64, x), numeric_data)
+    
+    # If there are more than five elements, keep only the last five
+    if length(float_data) > 5
+        float_data = float_data[end-3:end]
+    end
+
+    return float_data
 end
 
 """
-    parse_reset(line::Array{SubString{String},1})
+    view_data(portname::String, baudrate::Int)
 
-Parse a line of data values. Only the last four values are parsed. Resets the timestamp counter.
+Starts reading data from the serial port, parses the data, adds an elapsed time column in seconds, 
+and prints the data to the console.
 
 # Arguments
-- `line`: An array of substrings
+- `portname`: Name of the serial port (String)
+- `baudrate`: Baud rate for the serial communication (Int)
 
 # Returns
-- An array of parsed float values with timestamp as the first element
+- `nothing`
 """
-function parse_reset(line::Array{SubString{String},1})
-    global counter_reset
-    counter_reset = 1  # Reset the counter
-    parsed_values_reset = parse.(Float64, line[end-3:end])  # Convert to float array
-    result_reset = [counter_reset; parsed_values_reset]  # Create result array with counter as the first element
-    println("Result Reset: ", result_reset)  # Print the result
-    return result_reset
+function view_data(portname::String, baudrate::Int)
+    global continue_viewing = true
+    data_ch = SerialPortReader.start_reading(portname, baudrate)
+    start_time = now()
+    @async while continue_viewing
+        data = take!(data_ch)
+        parsed_data = parse_data(data)
+        elapsed_time = (now() - start_time).value / 1000
+        parsed_data = [elapsed_time; parsed_data]
+        @show parsed_data
+    end
 end
 
-# Create a circular buffer for the parse_reset function
-buffer_size_reset = 500  # Define the size of the buffer
-buffer_reset = CircularBuffer{Array{Float64,1}}(buffer_size_reset)
+"""
+    stop_viewing()
 
-# Function to add data from parse_reset function to the buffer and update the observable
-function add_data_to_reset_buffer(data)
-    push!(buffer_reset, data)
-    data_reset_observable[] = collect(buffer_reset)
+Stops the data viewing process.
+
+# Returns
+- `nothing`
+"""
+function stop_viewing()
+    global continue_viewing = false
+    SerialPortReader.stop_reading()
 end
 
-# Define a new parsing function that calls parse_reset and add_data_to_reset_buffer
-function parse_and_add_to_buffer(line::Array{SubString{String},1})
-    data_reset = parse_reset(line)
-    add_data_to_reset_buffer(data_reset)
-end
-
-# Create an observable for the data from parse_reset function
-data_reset_observable = Observable(collect(buffer_reset))
-
-# Create a plot that updates whenever the data from parse_reset function changes
-fig_reset = Figure()
-ax_reset = Axis(fig_reset[1, 1], xlabel = "Time (sec)", ylabel = "Temperature (°C)")
-line_reset = lines!(ax_reset, lift(d -> first.(d), data_reset_observable), lift(d -> d[:, 2], data_reset_observable), visible = lift(x -> length(x) > 0, data_reset_observable))
-
-display(fig_reset)
-
-# Start reading data from the serial port
-SerialPortReader.start_reading("COM9", 9600, parse_and_add_to_buffer)
