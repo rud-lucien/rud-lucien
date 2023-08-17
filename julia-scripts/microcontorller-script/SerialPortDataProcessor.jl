32325da156a12982a26455106740af5ebe1e4ca1
rud-lucien <30354417+rud-lucien@users.noreply.github.com>
2023-08-17 17:15:43 -0400

    enable start and stop reading
    created a global variable that enables me to stop and stop reading while com port is still open

--- julia-scripts/microcontorller-script/SerialPortDataProcessor.jl
+++ julia-scripts/microcontorller-script/SerialPortDataProcessor.jl
@@ -1,8 +1,12 @@
-# Import serial port package
+# Import necessary packages
 using LibSerialPort
 using Dates
 using DataFrames
-# Create a fuction called open_serial_port that takes portname and baudrate as arguments and opens the serial port.
+
+# Global flag to control the data reading process
+global continue_reading = false
+
+# Function to open a serial port given a port name and baud rate
 function open_serial_port(portname::String, baudrate::Int)::SerialPort
     try
         port = LibSerialPort.open(portname, baudrate)
@@ -13,14 +17,13 @@ function open_serial_port(portname::String, baudrate::Int)::SerialPort
     end
 end
 
-# Create a function called close_serial_port that closes the serial port opened by the open_serial_port function.
+# Function to close a serial port
 function close_serial_port(port::SerialPort)::Nothing
     close(port)
     return nothing
 end
 
-# Create a function called check_serial_port that checks if the serial port is open or closed. The function should return a boolean value. and print serial port open if its open and serila port not opened if its closed
-
+# Function to check if a serial port is open or closed
 function check_serial_port(port::SerialPort)::Bool
     if isopen(port)
         println("Serial port open")
@@ -31,25 +34,8 @@ function check_serial_port(port::SerialPort)::Bool
     end
 end
 
-
-
-#________________________________________________________________________________________________________________________
-#Test the fuctions that open and close serial ports
-#________________________________________________________________________________________________________________________
-# Call open_serial_port function to open the serial port
-#data_ch = open_serial_port("COM12", 9600)
-
-# Call check_serial_port function to check if the serial port is open
-#check_serial_port(data_ch)
-
-# Call close_serial_port function to close the serial port
-#close_serial_port(data_ch)
-
-# Call check_serial_port function to check if the serial port is closed
-#check_serial_port(data_ch)
-
-
-# Create a function called read_serial_data that reads data from the data_ch if its open and returns it as an array of strings, else it notifies the user
+# Function to read data from a serial port
+# It reads data continuously in an asynchronous loop
 function read_serial_data(port::SerialPort)::Union{Channel{Array{SubString{String},1}}, Nothing}
     if check_serial_port(port) == true
         data_ch = Channel{Array{SubString{String},1}}(1)
@@ -59,7 +45,6 @@ function read_serial_data(port::SerialPort)::Union{Channel{Array{SubString{Strin
             data_str = String(data)
             str_values = split(data_str)  # Split the string into an array of substrings
             str_values = str_values[2:end]  # Drop the first element
-            #@show str_values
             put!(data_ch, str_values)
             sleep(0.1)  # Pause to prevent high CPU usage
         end
@@ -71,8 +56,7 @@ function read_serial_data(port::SerialPort)::Union{Channel{Array{SubString{Strin
     end
 end
 
-
-#create a function called parse_data that takes an array of strings as an argument and returns an array of floats
+# Function to parse data from an array of strings to an array of floats
 function parse_data(data::Array{SubString{String},1})
     # Remove any non-numeric strings
     numeric_data = filter(x -> isdigit(x[1]) || x[1] == '.', data)
@@ -83,45 +67,52 @@ function parse_data(data::Array{SubString{String},1})
     return float_data
 end
 
+# Function to stop reading data for live plot
+function stop_reading_data_for_live_plot()
+    global continue_reading = false
+end
 
-function view_data_for_live_plot(incoming_data::Union{Channel{Array{SubString{String},1}}, Nothing}, df::DataFrame)
+# Function to start reading data for live plot
+function start_reading_data_for_live_plot(incoming_data::Union{Channel{Array{SubString{String},1}}, Nothing}, df::DataFrame)
+    global continue_reading = true
+
     if incoming_data == nothing
         println("No data channel available")
         return
     end
 
     start_time = now()
-    @async while true
+    @async while continue_reading
         data = take!(incoming_data)
         parsed_data = parse_data(data)
         elapsed_time = (now() - start_time).value / 1000
         parsed_data = [elapsed_time; parsed_data]
         push!(df, parsed_data)
+        sleep(0.1)  # Pause to prevent high CPU usage
     end
 end
 
-df_live = DataFrame(DataFrame(Time=Float64[], Humidity=Float64[], Temperature_C=Float64[], Temperature_F=Float64[], Heat_Index_C=Float64[], Heat_Index_F=Float64[])
-)
+# Define a DataFrame to store the live data
+df_live = DataFrame(DataFrame(Time=Float64[], Humidity=Float64[], Temperature_C=Float64[], Temperature_F=Float64[], Heat_Index_C=Float64[], Heat_Index_F=Float64[]))
 
 # Open the serial port and start reading data
 data_ch = open_serial_port("COM12", 9600)
 incoming_data = read_serial_data(data_ch)
-view_data_for_live_plot(incoming_data, df_live)
+
+# Start reading data for live plot
+start_reading_data_for_live_plot(incoming_data, df_live)
 
 # Wait for some data to be read
 sleep(5)
 
+# Stop reading data for live plot
+stop_reading_data_for_live_plot()
+
 # Close the serial port
 close_serial_port(data_ch)
+
+# Display the DataFrame in a table
 vscodedisplay(df_live)
-# Call the read_serial_data function
-#read_serial_data(data_ch)
 
-# Call the parse_live_data function
-view_data_for_live_plot(read_serial_data(data_ch))
-
-# Call the close_serial_port function to close the serial port
-close_serial_port(data_ch)
-
-# Call check_serial_port function to check if the serial port is closed
+# Check if the serial port is closed
 check_serial_port(data_ch)
