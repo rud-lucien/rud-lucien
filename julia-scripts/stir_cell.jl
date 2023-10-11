89ce105c344082591b84617952ccccd237c193f7
rud-lucien <30354417+rud-lucien@users.noreply.github.com>
2023-10-11 11:06:00 -0400

    added permeate volume in output

--- julia-scripts/stir_cell.jl
+++ julia-scripts/stir_cell.jl
@@ -23,19 +23,21 @@ function calculate_parameters(starting_volume, concentration_factor, max_stir_ce
 
     return (
         concentration_volume = concentration_volume,
+        permiate_volume = permiate_volume,
         total_diafiltration_volume = total_diafiltration_volume,
         total_number_of_df_steps = total_number_of_df_steps,
-        df_volume_per_step = df_volume_per_step,
+        df_volume_per_step = df_volume_per_step
     )
 end
 
 starting_volume = 50
-concentration_factor = 5
+concentration_factor = 4
 max_stir_cell_volume = 50
 
 results = calculate_parameters(starting_volume, concentration_factor, max_stir_cell_volume)
 
 println("Concentration Volume: ", results.concentration_volume)
+println("Permeate Volume: ", results.permiate_volume)
 println("Total Diafiltration Volume: ", results.total_diafiltration_volume)
 println("Total Number of Diafiltration Steps: ", results.total_number_of_df_steps)
 println("Diafiltration Volume per Step: ", results.df_volume_per_step)
