62456405b83d4a9a366c8fe9b23f3264d7fc6d71
rud-lucien <30354417+rud-lucien@users.noreply.github.com>
2024-06-21 10:49:37 -0400

    define a trough state function
    have a working function

--- bulk_dispense/src/main.cpp
+++ bulk_dispense/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Controllino.h>
 
-#define pin_sensor CONTROLLINO_A0
+#define TROUGH_STATE_SENSOR CONTROLLINO_A0
 
 void setup()
 {
@@ -9,60 +9,22 @@ void setup()
   // Start serial communication for debugging
   Serial.begin(115200);
 
- //Initialize the digital pin as an input
-  pinMode(pin_sensor, INPUT);
-
+  // Initialize the digital pin as an input
+  pinMode(TROUGH_STATE_SENSOR, INPUT);
 }
-int sensorState;
+
+bool troughState()
+{
+
+  // Read the state of the capacitive sensor
+  bool troughSensorState = digitalRead(TROUGH_STATE_SENSOR);
+  return troughSensorState;
+}
 
 void loop()
 {
-  // Read the state of the ca`pacitive sensor
-  sensorState = digitalRead(pin_sensor);
-
-  // Print the raw sensor state to the serial monitor
-  Serial.print("Raw sensor state: ");
-  Serial.println(digitalRead(pin_sensor));
-
-  // Print the sensor state to the serial monitor
-  if (sensorState == HIGH)
-  {
-    Serial.println("D0 is HIGH");
-  }
-  else
-  {
-    Serial.println("D0 is LOW");
-  }
-
-  // Small delay before next loop
-  delay(500);
+  // Print the state of trough sensor to the serial monitor 1=full, 0=false
+  Serial.print("trough state: ");
+  Serial.println(troughState());
+  delay(200);
 }
-
-
-
-// void setup()
-// {
-//   // Initialize the analog pin as an input
-//   pinMode(CONTROLLINO_A0, INPUT);
-
-//   // Start serial communication for debugging
-//   Serial.begin(115200);
-// }
-
-// void loop()
-// {
-//   // Read the analog value from the capacitive sensor
-//   int sensorValue = analogRead(CONTROLLINO_A0);
-
-//   // Convert the analog value to voltage
-//   float voltage = sensorValue * (5.0 / 1023.0);
-
-//   // Print the raw analog sensor value and the calculated voltage to the serial monitor
-//   Serial.print("Raw sensor value: ");
-//   Serial.print(sensorValue);
-//   Serial.print(" - Voltage: ");
-//   Serial.println(voltage);
-
-//   // Small delay before next loop
-//   delay(500);
-// }
